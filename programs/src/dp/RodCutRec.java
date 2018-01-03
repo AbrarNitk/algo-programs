c4f752d669a71f5a379f727baa7e29087f7f3262
Abrar Khan <abrar.nitk@gmail.com>
2018-01-04 00:09:51 +0530

    changes

--- programs/src/dp/RodCutRec.java
+++ programs/src/dp/RodCutRec.java
@@ -4,28 +4,43 @@ import java.io.File;
 import java.util.Scanner;
 
 public class RodCutRec {
-    int price[];
-    int n;
-    public RodCutRec(){
+    private int price[];
+    private int N;
 
-    }
-
-    public static void main(String[] args) {
-        System.out.println("args = [" +324975+ "]");
-        Scanner sc = readNumberFile("/home/john/IdeaProjects/algo-programs/programs/classes/input.txt");
-        int n = sc.nextInt();
-        while (sc.hasNext()){
-            System.out.println(sc.nextInt());
+    public RodCutRec(String filePath) {
+        Scanner sc = readNumberFile(filePath);
+        N = sc.nextInt();
+        price = new int[N+1];
+        for (int i = 1; i <= N && sc.hasNext(); i++) {
+            price[i] = sc.nextInt();
         }
     }
 
-    public static Scanner readNumberFile(String filePath) {
+    int rodCutRec() {
+        return 0;
+    }
+
+    int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + 324975 + "]");
+        String filePath = "/home/john/IdeaProjects/algo-programs/programs/classes/input.txt";
+        RodCutRec rodCutRec = new RodCutRec(filePath);
+    }
+
+    private static Scanner readNumberFile(String filePath) {
         try {
             File file = new File(filePath);
             return new Scanner(file);
         } catch (Exception e) {
             System.out.println("Exception in reading file and exception is :: " + e);
+            throw new RuntimeException("Exception is Here::"+e);
         }
-        return null;
     }
 }
