7bdd3d5b1c231d469765e03a10c2571dc24370dc
Abrar Khan <abrar.nitk@gmail.com>
2018-01-03 23:55:03 +0530

    changes

--- programs/src/dp/RodCutRec.java
+++ programs/src/dp/RodCutRec.java
@@ -1,7 +1,31 @@
 package dp;
 
-public class RodCutRec {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.Scanner;
 
+public class RodCutRec {
+    int price[];
+    int n;
+    public RodCutRec(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("args = [" +324975+ "]");
+        Scanner sc = readNumberFile("/home/john/IdeaProjects/algo-programs/programs/classes/input.txt");
+        int n = sc.nextInt();
+        while (sc.hasNext()){
+            System.out.println(sc.nextInt());
+        }
+    }
+
+    public static Scanner readNumberFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            return new Scanner(file);
+        } catch (Exception e) {
+            System.out.println("Exception in reading file and exception is :: " + e);
+        }
+        return null;
     }
 }
